8360ae877534ee84e4eee3956f7aeb6d05a580f1
zavarykin87 <zavarykin87@gmail.com>
2026-07-03 14:37:25 -0700

    поменял базовую верстку проекта

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import "./style.css";
 
 function initApp() {
-    
+    const rootEl = document.querySelector(".root");
+
+    if (!rootEl) {
+        throw new Error("Root element not found");
+    }
+
+    const h1El = document.createElement("h1");
+    h1El.textContent = "Welcome to the Weather App";
+    rootEl.appendChild(h1El);
+
 }
 
 initApp();
